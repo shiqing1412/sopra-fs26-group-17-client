634b58b9750099d7201c1a118e5c03fab1d4720d
yarablaser <yara_blaser@bluewin.ch>
2026-04-12 20:32:43 +0200

    added component LocationSearch for search and autocompletion using google places API #78

--- app/types/location.ts
+++ app/types/location.ts
@@ -1,6 +1,6 @@
 export interface Location {
-  id: number | null;
-  placeId: string | null;
+  id: number | null; // database ID
+  placeId: string | null; // Google Place ID
   name: string | null;
   latitude: number | null;
   longitude: number | null;
